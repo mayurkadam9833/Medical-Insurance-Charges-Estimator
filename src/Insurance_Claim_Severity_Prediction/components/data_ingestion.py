2824a084ed6fdcd03d329674ebf6b518c5b2eb9a
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-08-21 19:28:17 +0530

    write comments

--- src/Insurance_Claim_Severity_Prediction/components/data_ingestion.py
+++ src/Insurance_Claim_Severity_Prediction/components/data_ingestion.py
@@ -5,12 +5,15 @@ from src.Insurance_Claim_Severity_Prediction.logging import logger
 from src.Insurance_Claim_Severity_Prediction.entity.config_entity import DataIngestionConfig
 from src.Insurance_Claim_Severity_Prediction.utils.common import get_size
 
+# DataIngestion class -> used to download and extract dataset
 class DataIngestion:
     def __init__(self,config:DataIngestionConfig):
         self.config=config
 
+    # method to download dataset from url
     def download_file(self):
         if not os.path.exists(self.config.local_data_file):
+            # download file from source_url and save to local_data_file
             filename,header=urlretrieve(
                 url=self.config.source_url,
                 filename=self.config.local_data_file
@@ -18,11 +21,13 @@ class DataIngestion:
             logger.info(f"{self.config.local_data_file} download sucessfully from folllowing header:\n{header}")
 
         else:
+             # if file already exists, log the size of the file
             logger.info(f"{self.config.local_data_file} is already exits of size:{get_size(self.config.local_data_file)}")
 
+    # method to unzip dataset
     def extract_file(self):
         unzip_path=self.config.unzip_dir
-        os.makedirs(unzip_path,exist_ok=True)
+        os.makedirs(unzip_path,exist_ok=True) # create folder if not exists
         with zipfile.ZipFile(self.config.local_data_file,"r")as zipref:
-            zipref.extractall(unzip_path)
+            zipref.extractall(unzip_path)      # extract all files inside unzip_dir
 
